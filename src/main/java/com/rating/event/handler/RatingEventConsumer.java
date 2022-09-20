88c564731fa41d2872aa6ff42f36952aaf425b75
sabaja <jacoposabatini76@gmail.com>
2022-09-20 22:46:47 +0200

    Add update stars / non intercetta correttamente il messaggio di aggiornamento

--- src/main/java/com/rating/event/handler/RatingEventConsumer.java
+++ src/main/java/com/rating/event/handler/RatingEventConsumer.java
@@ -21,28 +21,40 @@ public class RatingEventConsumer {
     @RabbitListener(queues = "rating_status_request_queue", concurrency = "10")
     public RatingEventMessage receiveStatusRatingInformation(RatingDto ratingDto) {
         log.info("Server received a request of Rating information: {}", ratingDto);
-        return createRatingEventMessage(ratingDto);
+        final Long courseId = ratingDto.getCourseId();
+        if (Objects.nonNull(courseId)) {
+            final Rating rating = ratingRepository.findByCourseId(courseId);
+            if (Objects.nonNull(rating)) {
+                return createRatingEventMessage(rating);
+            }
+            log.error("No Rating found with id {}", courseId);
+        }
+        log.error("No Rating found ...");
+        return createDefaultRatingEventMessage();
     }
 
 
     @RabbitListener(queues = "rating_update_request_queue", concurrency = "10")
     public RatingEventMessage receiveUptadeRatingInformation(RatingDto ratingDto) {
         log.info("Server received a request of updating Rating information: {}", ratingDto);
-        RatingEventMessage message = new RatingEventMessage();
-        message.setRatingValue(ratingDto.getRatingValue());
-        message.setCourseId(ratingDto.getCourseId());
-        return message;
-    }
-    private RatingEventMessage createRatingEventMessage(RatingDto ratingDto) {
         final Long courseId = ratingDto.getCourseId();
         if (Objects.nonNull(courseId)) {
-            final Rating rating = ratingRepository.findByCourseId(courseId);
-            RatingEventMessage response = new RatingEventMessage();
-            response.setCourseId(courseId);
-            response.setRatingValue(rating.getRatingStars());
-            return response;
+            Rating rating = ratingRepository.findByCourseId(courseId);
+            if (Objects.nonNull(rating)) {
+                rating.setRatingStars(ratingDto.getRatingValue());
+                return createRatingEventMessage(ratingRepository.save(rating));
+            }
+            log.error("No Rating found with id {}", courseId);
         }
+        log.error("No Rating found ...");
         return createDefaultRatingEventMessage();
+    }
+
+    private RatingEventMessage createRatingEventMessage(Rating rating) {
+        RatingEventMessage message = new RatingEventMessage();
+        message.setRatingValue(rating.getRatingStars());
+        message.setCourseId(rating.getCourseId());
+        return message;
     }
 
     private RatingEventMessage createDefaultRatingEventMessage() {
