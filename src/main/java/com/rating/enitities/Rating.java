c7c96d8580dcf468b8f6e76e6df153ba004de82d
sabaja <jacoposabatini76@gmail.com>
2023-01-08 19:09:11 +0100

    Finish feature/migration_spring_boot_3

--- src/main/java/com/rating/enitities/Rating.java
+++ src/main/java/com/rating/enitities/Rating.java
@@ -1,11 +1,11 @@
 package com.rating.enitities;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.*;
 
 @Getter
 @Setter
