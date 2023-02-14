b4511da3fafdc678d332acee9be179064be9c4db
sabaja <jacoposabatini76@gmail.com>
2023-02-14 15:42:57 +0100

    Fix api endpoint

--- src/main/java/com/rating/controller/RatingController.java
+++ src/main/java/com/rating/controller/RatingController.java
@@ -2,7 +2,7 @@ package com.rating.controller;
 
 import com.rating.model.RatingDto;
 import com.rating.service.RatingService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -10,28 +10,29 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/ratings")
 public class RatingController {
 
-    @Autowired
-    private RatingService ratingService;
+
+    private final RatingService ratingService;
 
 
-    @PutMapping("/rating/update/{id}")
+    @PutMapping("/{id}")
     @ResponseBody
     public Mono<RatingDto> putRatingCourse(@PathVariable Long id, @RequestBody RatingDto dto) {
         RatingDto ratingDto = ratingService.saveOrUpdateRating(id, dto);
         return Mono.justOrEmpty(ratingDto);
     }
 
-    @GetMapping("/rating/{id}")
+    @GetMapping("/{id}")
     @ResponseBody
     public Mono<RatingDto> getRatingCourse(@PathVariable Long id) {
         final RatingDto ratingDto = ratingService.get(id);
         return Mono.justOrEmpty(ratingDto);
     }
 
-    @GetMapping("/rating/all")
+    @GetMapping("/")
     @ResponseBody
     public Flux<RatingDto> getAllRatingCourse() {
         final List<RatingDto> ratingDto = ratingService.getAll();
